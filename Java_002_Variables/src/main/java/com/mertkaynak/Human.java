ead31aaa1fbdb1c779d5de112e8a2dcc9211c286
Mert Kaynak <kaynak.mert35@gmail.com>
2023-11-19 20:07:03 +0300

    Java-002 - Variables
    Variables

---
+++ Java_002_Variables/src/main/java/com/mertkaynak/Human.java
@@ -0,0 +1,20 @@
+package com.mertkaynak;
+
+public class Human { // scope (kapsam)
+
+
+
+    public static void main(String[] args) {
+
+        String name = "Mert";
+        String surname = "Kaynak";
+        int kilo = 78;
+        double size = 1.80;
+
+        System.out.println("Adı: " + name);
+        System.out.println("Soyad: " + surname);
+        System.out.println("Kilo: " + kilo);
+        System.out.println("Boy: " + size);
+        System.out.println();
+    }
+}
---
+++ Java_002_Variables/src/main/java/com/mertkaynak/Variables.java
@@ -0,0 +1,12 @@
+package com.mertkaynak;
+
+public class Variables {
+
+    public static void main(String[] args) {
+        int myInt = 1;
+        double sayi = 1.234D;
+        float sayi2 = 1.2323F;
+        char myChar = 'A';
+        boolean myBoolean = true;
+    }
+}
