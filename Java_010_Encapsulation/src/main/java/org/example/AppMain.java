9898fe691712ea6e0effcec46694109c10dec988
Mert Kaynak <kaynak.mert35@gmail.com>
2023-11-25 19:06:54 +0300

    OOP - Encapsulation

---
+++ Java_010_Encapsulation/src/main/java/org/example/AppMain.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class AppMain {
+    public static void main(String[] args) {
+
+        Musteri musteri = new Musteri(
+                "Ali",
+                "Yağız",
+                "TR123123213",
+                1241241414,
+                30000.15);
+        System.out.println(musteri);
+    }
+}
