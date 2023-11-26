d74ed3531b2559649ccacbe82b17c986a27e97f5
Mert Kaynak <kaynak.mert35@gmail.com>
2023-11-26 20:20:51 +0300

    Collections

--- Java_016_Collections/src/main/java/com/mertkaynak/AppMain.java
+++ Java_016_Collections/src/main/java/com/mertkaynak/AppMain.java
@@ -1,9 +1,24 @@
 package com.mertkaynak;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AppMain {
     public static void main(String[] args) {
 
-        String<>
+        List myList1 = new ArrayList();
+        myList1.add("Mehmet");
+        myList1.add(10);
+
+        List <Integer> myList2 = new ArrayList();
+        myList2.add(15);
+
+        System.out.println("Liste 1: : " + myList1);
+        System.out.println("Liste 2: : " + myList2);
+
+        System.out.println("Liste 1: : " + myList1.isEmpty());
+        System.out.println("Liste 1: : " + myList1.contains("Mehmet"));
+
 
     }
 }
