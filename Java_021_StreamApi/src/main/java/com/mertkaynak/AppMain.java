e3ae12b1e1351893281c137c0e6c0b16c18d844c
Mert Kaynak <kaynak.mert35@gmail.com>
2023-12-02 21:36:45 +0300

    Stream API - Lambda

---
+++ Java_021_StreamApi/src/main/java/com/mertkaynak/AppMain.java
@@ -0,0 +1,27 @@
+package com.mertkaynak;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+public class AppMain {
+    public static void main(String[] args) {
+        Stream<String> bosKayit = Stream.empty();
+        System.out.println("Boş Kayıt: "+bosKayit);
+
+        Stream<String> tekKayit = Stream.of("Yakup");
+        System.out.println("Kayıt Bilgisi: "+tekKayit);
+
+        Stream<String> cokluKayit = Stream.of("Ali", "Mahir", "Yalçın", "Yunus", "Senem");
+        System.out.println("Kayıt Bilgileri: "+cokluKayit);
+
+        List<String> ogrenciListesi = List.of("Ali", "Mahir", "Yalçın", "Yunus", "Senem");
+        System.out.println("Öğrenci Bilgileri: "+ogrenciListesi);
+
+        System.out.println("--------------------------------");
+
+        Stream <String> ogrencilerStream = ogrenciListesi.stream();
+        System.out.println("Toplam Öğrenci Sayısı: "+ogrencilerStream.count());
+        ogrencilerStream.forEach(ogrenci -> System.out.println(ogrenci));
+        ogrencilerStream.forEach(System.out::println);
+    }
+}
