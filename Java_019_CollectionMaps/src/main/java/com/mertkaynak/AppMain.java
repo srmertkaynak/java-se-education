9a2832f966d1e75c56cc70d4a0ba6f9a6b7043a9
Mert Kaynak <kaynak.mert35@gmail.com>
2023-12-02 19:23:35 +0300

    Maps

--- Java_019_CollectionMaps/src/main/java/com/mertkaynak/AppMain.java
+++ Java_019_CollectionMaps/src/main/java/com/mertkaynak/AppMain.java
@@ -1,7 +1,25 @@
 package com.mertkaynak;
 
+import java.util.Map;
+import java.util.TreeMap;
+
 public class AppMain {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Map<String, Integer> plakaListe = new TreeMap();
+        plakaListe.put("Konya", 42);
+        plakaListe.put("Adana", 01);
+        plakaListe.put("Eskişehir", 26);
+        plakaListe.put("Kars", null);
+        plakaListe.put("Gaziantep", 27);
+        plakaListe.put("İzmir", 35);
+        plakaListe.put("Ağrı", 04);
+        plakaListe.put("Yalova", 77);
+        plakaListe.put("İstanbul", 34);
+
+        System.out.println(plakaListe);
+
+        for (String key : plakaListe.keySet()){
+            System.out.println("Plaka bilgisi: " + key + " " + plakaListe.get(key));
+        }
     }
 }
