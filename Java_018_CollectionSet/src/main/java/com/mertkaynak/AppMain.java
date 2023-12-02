e5aa21a67fe27ec8f104a37b688d689546f60b25
Mert Kaynak <kaynak.mert35@gmail.com>
2023-12-02 17:28:49 +0300

    Collection Set

---
+++ Java_018_CollectionSet/src/main/java/com/mertkaynak/AppMain.java
@@ -0,0 +1,11 @@
+package com.mertkaynak;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AppMain {
+    public static void main(String[] args) {
+        List ogrenciListe = new ArrayList();
+        ogrenciListe.add(100);
+    }
+}
