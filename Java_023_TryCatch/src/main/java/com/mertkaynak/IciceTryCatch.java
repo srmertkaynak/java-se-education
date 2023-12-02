0ec6a80d5364b0b7778ead7e0f64e82c8ed3e0f2
Mert Kaynak <kaynak.mert35@gmail.com>
2023-12-02 23:53:50 +0300

    İç İçe Try Catch

---
+++ Java_023_TryCatch/src/main/java/com/mertkaynak/IciceTryCatch.java
@@ -0,0 +1,27 @@
+package com.mertkaynak;
+
+public class IciceTryCatch {
+    public static void main(String[] args) {
+        try {
+            try {
+                try {
+                    try {
+                        try {
+
+                        }catch (){
+
+                        }
+                    }catch (){
+
+                    }
+                }catch (){
+
+                }
+            }catch (){
+
+            }
+        }catch (){
+
+        }
+    }
+}
