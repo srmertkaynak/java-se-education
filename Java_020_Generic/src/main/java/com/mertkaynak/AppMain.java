6b21ca047d85662e302d5c6d10336c8990072ba3
Mert Kaynak <kaynak.mert35@gmail.com>
2023-12-02 20:05:54 +0300

    Generic

---
+++ Java_020_Generic/src/main/java/com/mertkaynak/AppMain.java
@@ -0,0 +1,14 @@
+package com.mertkaynak;
+
+import java.util.ArrayList;
+
+public class AppMain {
+    public static void main(String[] args) {
+
+        ArrayList myArrayList1 = new ArrayList<>();
+        ArrayList<Integer> myArrayList2 = new ArrayList<>();
+
+        ListManager<String> myListManager = new ListManager();
+
+    }
+}
---
+++ Java_020_Generic/src/main/java/com/mertkaynak/ListManager.java
@@ -0,0 +1,8 @@
+package com.mertkaynak;
+
+import java.util.ArrayList;
+
+public class ListManager <T>{
+
+    private ArrayList<T> myList = new ArrayList<>();
+}
