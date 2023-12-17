dc7fed6d9775130169e024220146d8da3744d233
Mert Kaynak <kaynak.mert35@gmail.com>
2023-12-17 21:27:26 +0300

    Hibernate

---
+++ Java_025_JDBC_JavaDatabaseConnectivity/src/main/java/com/mertkaynak/MyCrud.java
@@ -0,0 +1,13 @@
+package com.mertkaynak;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class MyCrud {
+
+    private Connection connection = null;
+    private Statement statement = null;
+    private ResultSet resultset = null;
+
+}
---
+++ Java_026_Hibernate/src/main/java/com/mertkaynak/util/HibernateUtil.java
@@ -0,0 +1,4 @@
+package com.mertkaynak.util;
+
+public class HibernateUtil {
+}
